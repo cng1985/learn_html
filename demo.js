5ae2314e9f249125c5d3523227e92901f7ee88d8
cng1985 <735529126@qq.com>
2020-02-21 14:44:47 +0800

    go

--- demo.js
+++ demo.js
@@ -1,17 +1,17 @@
-var a=null;
-var b=undefined;
-console.log(a==b);
+var a = null;
+var b = undefined;
+console.log(a == b);
 console.log(a);
 console.log(b);
 
-function SpannableUtil(){
-    this.str="";
-    this.append=function(item){
-      this.str = this.str+item;
-    }
-    return this;
+function SpannableUtil() {
+  this.str = "";
+  this.append = function (item) {
+    this.str = this.str + item;
   }
-  var SpannableUtil=new SpannableUtil();
-  SpannableUtil.append("hi");
-  console.info(SpannableUtil.str);
-  console.info(typeof SpannableUtil);
+  return this;
+}
+var SpannableUtil = new SpannableUtil();
+SpannableUtil.append("hi");
+console.info(SpannableUtil.str);
+console.info(typeof SpannableUtil);
